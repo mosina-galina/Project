4e866b6801397b4694e0fe4b661f4166e5663cfc
Galina <galinka-ver@mail.ru>
2023-02-19 12:34:10 +0500

    Добавлен метод для нахождения строк меньше 4

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ int n = int.Parse(Console.ReadLine() ?? "");
 string [] m = new string [n];
 string [] m1 = new string [n];
 NewArray(m);
+Console.Write("Исходный массив ");
 WriteArray(m);
+ResultArray(m,m1);
+Console.WriteLine();
+Console.Write("Результат ");
+WriteArray(m1);
 void NewArray (string [] arr)
 {
     for (int i = 0; i < n; i++)
@@ -16,6 +21,19 @@ void WriteArray (string [] arr)
     for (int i = 0; i < n; i++)
     {
         Console.Write($"{arr[i]} ");
+
+    }
+}
+void ResultArray (string [] arr1, string [] arr2)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr1[i].Length < 4 )
+        {
+            arr2[j] = arr1[i];
+            j=j+1;
+        }
 
     }
 }
