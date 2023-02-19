b81c30724d8118bc55372a0ae78a84e82043b56a
Galina <galinka-ver@mail.ru>
2023-02-19 12:25:47 +0500

    Добавлены методы на ввод и вывод массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.WriteLine("Введите длинну массива ");
+int n = int.Parse(Console.ReadLine() ?? "");
+string [] m = new string [n];
+string [] m1 = new string [n];
+NewArray(m);
+WriteArray(m);
+void NewArray (string [] arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = Console.ReadLine() ?? "";
+    }
+}
+void WriteArray (string [] arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{arr[i]} ");
+
+    }
+}
